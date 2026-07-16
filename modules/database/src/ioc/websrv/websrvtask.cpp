ea8ce5626f7356d1595088384073eccbb7d09a87
Dimitra Pando <dpando@bnl.gov>
2026-07-16 14:35:39 -0400

    Added PV handling functions, added thread, updated report route

--- modules/database/src/ioc/websrv/websrvtask.cpp
+++ modules/database/src/ioc/websrv/websrvtask.cpp
@@ -33,7 +33,7 @@
 #include "osiSock.h"
 #include "taskwd.h"
 #include "cantProceed.h"
-
+#include "epicsThread.h"
 #include "epicsExport.h"
 
 #define epicsExportSharedSymbols
@@ -43,32 +43,36 @@
 #include "db_field_log.h"
 #include "dbServer.h"
 
+#include <dbAccess.h>
+#include <dbDefs.h>
+#include <dbStaticLib.h>
 
 #include <drogon/drogon.h>
 
 #define GLBLSOURCE
 #include "websrv.h"
 
-
-
+extern DBBASE *pdbbase;
 
 epicsThreadPrivateId webserverCurrentClient = 0;
 
 static std::string webserver_report_body(unsigned level);
+static void webserver_thread(void *);
+
+static Json::Value websrv_record_handling(const std::string& recordName);
+static Json::Value websrv_field_get(const std::string& recordName, const std::string& fieldName);
+static Json::Value websrv_field_put(const std::string& recordName, const std::string& fieldName, const std::string& fieldData);
+
+static const char *websrvAddress = "127.0.0.1";
+static const unsigned websrvPort = 8080;
 
 int webserver_client_initiating_current_thread ( char * pBuf, size_t bufSize )
 {
 
 }
 
-/*
- * webserver_init ()
- */
-
 static void webserver_init(void)
 {
-    printf("Pre webserver initialization!\n");
-
     drogon::app()
         .addListener("127.0.0.1", 8080)
         .registerHandler("/init",[](const drogon::HttpRequestPtr& req,std::function<void(const drogon::HttpResponsePtr&)>&& callback)
@@ -80,25 +84,6 @@ static void webserver_init(void)
             },
             {drogon::Get}
         )
-
-
-        .registerHandler("/report", [](const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback)
-            {
-
-            unsigned level = 1;
-            auto levelParam = req->getParameter("level");
-            if (!levelParam.empty()) {
-                level = std::stoul(levelParam);
-            }
-
-            auto resp = drogon::HttpResponse::newHttpResponse();
-            resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
-            resp->setBody(webserver_report_body(level));
-            callback(resp);
-            }
-        )
-
-
 
         .registerHandler("/stats", [](const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback)
             {
@@ -115,83 +100,216 @@ static void webserver_init(void)
                 callback(resp);
             },
             {drogon::Get}
+        )
+         
+        .registerHandler("/report", [](const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback)
+            {
+            unsigned level = 1;
+            auto levelParam = req->getParameter("level");
+            if (!levelParam.empty()) {
+                level = std::stoul(levelParam);
+            }
+
+            auto resp = drogon::HttpResponse::newHttpResponse();
+            resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
+            resp->setBody(webserver_report_body(level));
+            callback(resp);
+            }
+        )
+
+        // Record handling route
+        .registerHandler("/{1}", [](const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, const std::string& recordName)
+            {
+                Json::Value result = websrv_record_handling(recordName);
+
+                auto resp = drogon::HttpResponse::newHttpResponse();
+                resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
+                resp->setStatusCode(drogon::k200OK);
+                resp->setBody(result["field"].asString() + ": " + result["field_data"].asString() + "\n");
+                callback(resp);
+            },
+            {drogon::Get}
+        )
+
+        // Field handling route (gets and puts field data)
+        .registerHandler("/{1}/{2}", [](const drogon::HttpRequestPtr& req, std::function<void(const drogon::HttpResponsePtr&)>&& callback, const std::string& recordName, const std::string& fieldName)
+            {
+                Json::Value result;
+
+                // If a field name is not specified, returns a 404 Not Found
+                if (fieldName.empty()) {
+                    auto resp = drogon::HttpResponse::newHttpResponse();
+                    resp->setStatusCode(drogon::k404NotFound);
+                    resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
+                    resp->setBody("404 Not Found: field name is missing\n");
+                    callback(resp);
+                    return;
+                }
+                // If the request method is GET
+                if (req->method() == drogon::Get) {
+                    result = websrv_field_get( recordName, fieldName);
+                }
+
+                // If the request method is PUT
+                else if (req->method() == drogon::Put) {const std::string fieldData = req->getParameter("field_data");
+
+                    if (fieldData.empty()) 
+                    {
+                        result["record"] = recordName;
+                        result["field"] = fieldName;
+                        result["error"] = "No field_data specified!";
+                    }
+                    else {
+                        result = websrv_field_put(recordName, fieldName, fieldData);
+                    }
+                }
+                else {
+                    result["error"] = "Error! - Unsupported HTTP method - Please use GET or PUT\n";
+                }
+
+                auto resp = drogon::HttpResponse::newHttpResponse();
+
+                if (result.isMember("error")) 
+                {
+                    resp->setStatusCode(drogon::k400BadRequest);
+                }
+                else 
+                {
+                    resp->setContentTypeCode(drogon::CT_TEXT_PLAIN);
+                    resp->setStatusCode(drogon::k200OK);
+                    resp->setBody(result["field"].asString() + ": " + result["field_data"].asString() + "\n");
+                }
+
+                callback(resp);
+            },
+            {drogon::Get, drogon::Put}
         );
 
-    drogon::app().run(); //runs the actual server 
+        /*
+        // Background Thread
+        // Runs the Drogon web server in a separate thread to avoid blocking the main EPICS thread
+        */
+
+        printf("Creating webserver background run thread!\n");
+
+        epicsThreadId threadId = epicsThreadCreate("webserver", epicsThreadPriorityMedium, epicsThreadGetStackSize(epicsThreadStackMedium), webserver_thread, nullptr);
+
+        // If thread could not be created, print error message
+        if (!threadId) {
+            errlogPrintf("Drogon web server thread has failed to be created\n");
+            return;
+        }
+
+    printf("Background thread running! Webserver successfully initialized!\n");
 }
 
-static
-void webserver_run (void)
+static void webserver_thread(void *)
 {
-    websrvTCP_ctl = ctlRun;
- 
+    drogon::app().run();
 }
 
-static
-void webserver_pause (void)
+static Json::Value websrv_record_handling(const std::string& recordName)
 {
-    //beacon_ctl = ctlPause;
-    websrvTCP_ctl = ctlPause;
+    return websrv_field_get(recordName, "VAL");
 }
 
+static Json::Value websrv_field_get(const std::string& recordName, const std::string& fieldName)
+{
+    Json::Value result;
 
+    result["record"] = recordName;
+    result["field"] = fieldName;
 
+    const std::string fullName = recordName + "." + fieldName;
 
-static const char *websrvAddress = "127.0.0.1";
-static const unsigned websrvPort = 8080;
+    DBADDR addr;
+
+    long status = dbNameToAddr(fullName.c_str(), &addr);
+
+    if (status) {
+        result["error"] = "Record or field not found";
+        return result;
+    }
+
+    char fieldData[MAX_STRING_SIZE] = {};
+
+    status = dbGetField( &addr, DBR_STRING, fieldData, nullptr, nullptr, nullptr);
+
+    if (status) {
+        result["error"] = "Unable to read field";
+        return result;
+    }
+
+    result["field_data"] = fieldData;
+
+    return result;
+}
+
+static Json::Value websrv_field_put(const std::string& recordName, const std::string& fieldName, const std::string& fieldData)
+{
+    Json::Value result;
+
+    result["record"] = recordName;
+    result["field"] = fieldName;
+
+    const std::string fullName = recordName + "." + fieldName;
+
+    DBADDR addr;
+
+    long status = dbNameToAddr(fullName.c_str(), &addr);
+
+    if (status) {
+        result["error"] = "Record or field not found";
+        return result;
+    }
+
+    status = dbPutField(&addr, DBR_STRING, fieldData.c_str(), 1);
+
+    if (status) {
+        result["error"] = "Unable to write field";
+        return result;
+    }
+
+    result["field_data"] = fieldData;
+
+    return result;
+}
 
 static std::string webserver_report_body(unsigned level)
 {
     std::ostringstream body;
-
     
     if (level == 0) {
         body << "HTTP Web Server\n";
         body << "Framework: Drogon\n";
     }
    
-
-    
     if (level == 1) {
         body << "Listening address: " << websrvAddress << "\n";
         body << "Listening port: " << websrvPort << "\n";
-
     }
 
     if (level >= 2) {
         body << "Routes:\n";
-        body << "  GET /report\n";
+        body << "/init\n";
+        body << "/stats\n";
+        body << "/report\n";
+        body << "/{record}\n";
+        body << "/{record}/{field}\n";
     }
 
     return body.str();
 }
 
-
-/*
- *  webserver_report()
- */
 void webserver_report (unsigned level)
 {   
-
     printf("%s", webserver_report_body(level).c_str());
-
-
-
 }
-
-
-
-
-
-
-
-
-
 
 void webserver_stats ( unsigned *pChanCount, unsigned *pCircuitCount )
 {
-    // circuit count --> active http connection count
     unsigned int circuitCount = static_cast<unsigned>(drogon::app().getConnectionCount());
+    
     if (circuitCount < 0) {
         *pCircuitCount = 0;
     }
@@ -200,11 +318,19 @@ void webserver_stats ( unsigned *pChanCount, unsigned *pCircuitCount )
     }
 
     printf("Active HTTP connections: %u\n", circuitCount);
-
-
-
 }
 
+static
+void webserver_run (void)
+{
+    websrvTCP_ctl = ctlRun;
+}
+
+static
+void webserver_pause (void)
+{
+    websrvTCP_ctl = ctlPause;
+}
 
 static dbServer webserver = {
     ELLNODE_INIT,
